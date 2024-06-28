99fe6d0b07683e5276a5553501a5be99cbd63c15
Ali-Owieda92 <owiedaali92@gmail.com>
2024-06-28 11:45:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import TodoItems from './component/TodoItems/Todoitems';
+import AddItem from './component/AddItems/AddItem';
 
-function App() {
+
+class App extends Component {
+
+  state = {
+    items : [
+      {id:1, name:'Ali', age:22},
+      {id:2, name:'Mohamed', age:23},
+      {id:3, name:'Abdo', age:24},
+    ]
+  }
+
+  deleteItem = (id) => {
+    // let items = this.state.items;
+    // let i = items.findIndex(item => item.id === id);
+    // items.splice(i,1);
+    // this.setState({items});
+
+    let items = this.state.items.filter(item => {
+      return item.id !== id;
+    });
+    this.setState({items});
+  }
+
+  addItem = (item) => {
+    item.id = Math.random();
+    let items = this.state.items;
+    items.push(item);
+    this.setState({items})
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+        <h1 className='text-center'>TodoList App</h1>
+        <TodoItems items={this.state.items} deleteItem={this.deleteItem}/>
+        <AddItem addItem={this.addItem}/>
     </div>
   );
+  }
 }
 
 export default App;
